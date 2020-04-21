01eca9186e5fc58684af0ef2799e3620a39721ea
Marcos Cordeiro <marcoscbrito@gmail.com>
2020-04-21 10:14:00 -0300

    Update wiki.py
    removing english domain

--- Projeto/Script/wiki.py
+++ Projeto/Script/wiki.py
@@ -4,7 +4,7 @@ from sseclient import SSEClient as EventSource
 
 my_stream_name = 'WikiStream'
 kinesis_client = boto3.client('firehose', region_name='us-east-1', aws_access_key_id='AKIAR3CZLWEYF7GITSGS' ,aws_secret_access_key='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')
-domains = ['pt.wikipedia.org','en.wikipedia.org']
+domains = ['pt.wikipedia.org']
 url = 'https://stream.wikimedia.org/v2/stream/recentchange'
 
 for event in EventSource(url):
